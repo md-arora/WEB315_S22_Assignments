ea5e862e332c71072a75de3ea03681b12d27b72f
Mohit <105736945+md-arora@users.noreply.github.com>
2022-06-04 12:33:01 -0400

    configuered to use sqlite in development and sqlserver in production

--- MDsSuperBikes/Startup.cs
+++ MDsSuperBikes/Startup.cs
@@ -14,20 +14,29 @@ namespace MDsSuperBikes
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if(Environment.IsDevelopment()){
+                services.AddDbContext<RazorPagesBikeContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesBikeContext")));
+            } else {
+                services.AddDbContext<RazorPagesBikeContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("RazorPagesBikeContext")));
+            }
+
             services.AddRazorPages();
 
-            services.AddDbContext<RazorPagesBikeContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesBikeContext")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
