a4184bd6dc9e276ecf20d611c7e86ac56513a4cc
= <=>
2022-06-25 19:09:12 -0400

    Added search form , Serching data by Model name

--- MDsSuperBikes/Pages/Bikes/Index.cshtml.cs
+++ MDsSuperBikes/Pages/Bikes/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesBike.Models;
 
@@ -19,10 +20,22 @@ namespace MDsSuperBikes.Pages_Bikes
         }
 
         public IList<Bike> Bike { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList CompanyName { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BikeCompanyName { get; set; }
 
         public async Task OnGetAsync()
         {
-            Bike = await _context.Bike.ToListAsync();
+            var bikes = from b in _context.Bike
+                 select b;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                bikes = bikes.Where(s => s.ModelName.Contains(SearchString));
+            }
+
+            Bike = await bikes.ToListAsync();
         }
     }
 }
