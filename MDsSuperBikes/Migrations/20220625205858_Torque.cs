ae66233a4d3cff789818a499129332e8e35dc062
= <=>
2022-06-25 17:21:41 -0400

    Added migration for new Model fields

---
+++ MDsSuperBikes/Migrations/20220625205858_Torque.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MDsSuperBikes.Migrations
+{
+    public partial class Torque : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TopSpeed",
+                table: "Bike",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Torque",
+                table: "Bike",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TopSpeed",
+                table: "Bike");
+
+            migrationBuilder.DropColumn(
+                name: "Torque",
+                table: "Bike");
+        }
+    }
+}
